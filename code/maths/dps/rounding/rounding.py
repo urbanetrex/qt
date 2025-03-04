cd88e457bcd7c0e51c4c0757c929c4dd775b85f3
urbanetrex <urbanetrex@gmail.com>
2025-03-04 22:30:47 +1100

    Update rounding.py

--- code/maths/dps/rounding/rounding.py
+++ code/maths/dps/rounding/rounding.py
@@ -2,6 +2,6 @@ n=int(input("Enter a number!"))
 round_units=int(input("Enter the units that you want to round, eg. 10, 100, 99..."))
 mod=n%round_units
 if mod*2<round_units:
-  print("Round down to ", end='')
+  print("Round down to "+str((n//round_units)*round_units))
 else:
-  print("Round up to ", end='')
+  print("Round up to "+str((n//round_units+1)*round_units))
